e8decaef6c8ade04c6a8847d56d519cdda99af6b
Ale <alejohan1130@gmail.com>
2024-05-04 06:49:59 -0500

    add
    avance

--- Front-end/tailwind.config.mjs
+++ Front-end/tailwind.config.mjs
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    colors: {
+      principalGreen: "#729517",
+	  secondaryGreen: "#577004",
+	  links: "#FEEF08"
+    },
+    extend: {},
+  },
+  plugins: [],
+};
