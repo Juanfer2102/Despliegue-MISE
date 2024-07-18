23110ad0fbf983fd1b0df83a4d4d000e5c2aa79f
Aleej0 <160734369+Aleej0@users.noreply.github.com>
2024-07-18 08:41:00 -0500

    update

--- Front-end/src/components/pruebas/visualizer.jsx
+++ Front-end/src/components/pruebas/visualizer.jsx
@@ -1,16 +1,20 @@
 import React from "react";
 
-export const visualizer = (resultprop) => {
+export const Visualizer = ({ resultprop, classAll, objClass, itemClass }) => {
   return (
     <>
-    {console.log(typeResult)}
-      <div>
-        {resultprop.map((typeResult) => (
-          <div key={typeResult.id_rol}>
-            <h1>{typeResult.id_rol}</h1>
-            <p>{typeResult.descripcion}</p>
-          </div>
-        ))}
+      {console.log(resultprop)}
+      <div className={`${classAll}`}>
+        {Array.isArray(resultprop) &&
+          resultprop.map((item, index) => (
+            <div key={index} className={`${objClass}`}>
+              {Object.entries(item).map(([key, value]) => (
+                <div key={key} className={`${itemClass}`}>
+                  {key}: {value}
+                </div>
+              ))}
+            </div>
+          ))}
       </div>
     </>
   );
