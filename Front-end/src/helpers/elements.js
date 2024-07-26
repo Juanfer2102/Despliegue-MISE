61e6b95dd31b6183b5ca382f1e353bd7b1e8b1c4
Aleej0 <alejohan1130@gmail.com>
2024-07-26 06:54:04 -0500

    a

---
+++ Front-end/src/helpers/elements.js
@@ -0,0 +1,7 @@
+export const Elementoscript = [
+    {icon:"fa-solid fa-objects-column", texto:"Dashboard"},
+    {icon:"fa-solid fa-building", texto:"Empresas Registradas"},
+    {icon:"fa-solid fa-table-cells", texto:"Modulos"},
+    {icon:"fa-solid fa-users", texto:"Cambiar contraseña"},
+    {icon:"fa-solid fa-gear", texto:"Crear estudiante"},
+  ]
