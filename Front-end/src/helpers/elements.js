a6a27f63e98dd34cde531cea51fc384658a37f57
Aleej0 <alejohan1130@gmail.com>
2024-07-26 12:06:24 -0500

    Matenme

--- Front-end/src/helpers/elements.js
+++ Front-end/src/helpers/elements.js
@@ -1,7 +1,5 @@
 export const Elementoscript = [
-    {icon:"fa-solid fa-objects-column", texto:"Dashboard"},
-    {icon:"fa-solid fa-building", texto:"Empresas Registradas"},
-    {icon:"fa-solid fa-table-cells", texto:"Modulos"},
-    {icon:"fa-solid fa-users", texto:"Cambiar contraseña"},
-    {icon:"fa-solid fa-gear", texto:"Crear estudiante"},
+    {icon:"fa-solid fa-bars", texto:"Dashboard", URL:"/empresasRegistradas"},
+    {icon:"fa-solid fa-building", texto:"Empresas Registradas", URL:"/empresasRegistradas"},
+    {icon:"fa-solid fa-table-cells", texto:"Modulos", URL:"/modulos"},
   ]
---
+++ Front-end/src/helpers/elementsadmin.js
@@ -0,0 +1,4 @@
+export const Elementoscriptadmin = [
+    {icon:"fa-solid fa-users", texto:"Usuarios Registrados", URL:"/visualizarUsers"},
+    {icon:"fa-solid fa-gear", texto:"Ajustes", URL:"/visualizarUsers"},
+]
