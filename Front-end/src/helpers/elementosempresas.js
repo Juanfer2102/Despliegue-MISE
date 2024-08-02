9f9eb48c2098ed23732d507459573b878120abe5
Andryy <andreylenis2015@gmail.com>
2024-08-02 10:39:49 -0500

    todo arreglado dios mio ya

--- Front-end/src/helpers/elementosempresas.js
+++ Front-end/src/helpers/elementosempresas.js
@@ -1,5 +1,6 @@
 export const Elementosempresas = [
-    {empresaicon:"fa-solid fa-bars", tipoTarjeta:"Dashboard", numeroEmpresas:85},
-    {empresaicon:"fa-solid fa-bars", tipoTarjeta:"Dashboard", numeroEmpresas:55},
-    {empresaicon:"fa-solid fa-bars", tipoTarjeta:"Dashboard", numeroEmpresas:5},
-  ]
+  {empresaicon:"fa-solid fa-building-user", numeroEmpresas: 85, tipoTarjeta:"Empresas registradas"},
+  {empresaicon:"fa-solid fa-building-circle-check", numeroEmpresas: 20, tipoTarjeta:"Nuevas empresas"},
+  {empresaicon:"fa-solid fa-building-circle-exclamation", numeroEmpresas: 12, tipoTarjeta:"Nuevas solicitudes"},
+  {empresaicon:"fa-solid fa-building-user", numeroEmpresas: 45, tipoTarjeta:"Empresas registradas"}
+]
