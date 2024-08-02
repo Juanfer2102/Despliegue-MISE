5e7eb6b0c0821aefe7451d4795f183ee5232b6d5
Andryy <andreylenis2015@gmail.com>
2024-08-02 09:08:48 -0500

    Revert "yAAAAAAAAAAA"
    This reverts commit 91cf99279a4618b09b2671e29d8c74a6f65351f4.

--- Front-end/src/components/inputs/TextArea.jsx
+++ Front-end/src/components/inputs/TextArea.jsx
@@ -8,7 +8,7 @@ export const TextareaHero = ({DataType, width, widthInput, height,}) => (
         <p class="font-semibold">{DataType}</p>
     </div>
     <div>
-        <Textarea placeholder="Escribe aqui..." className={`${widthInput} ${height} mx-auto max-w-xs rounded-md hover:bg-transparent focus:border-white focus:border-solid border-solid border-2 border-white bg-transparent overflow-y-auto custom-scrollbar`} />
+        <Textarea placeholder="Escribe aqui..." className=`${widthInput} ${height} mx-auto max-w-xs rounded-md hover:bg-transparent focus:border-white focus:border-solid border-solid border-2 border-white bg-transparent overflow-y-auto custom-scrollbar` />
     </div>
     
 </div>
