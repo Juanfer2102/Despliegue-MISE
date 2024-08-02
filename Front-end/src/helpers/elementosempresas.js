67dff3a49c3d1bad52e2bb45612abda94c8893fe
Andryy <andreylenis2015@gmail.com>
2024-08-02 09:33:54 -0500

    aaaaaaaaaa

---
+++ Front-end/src/helpers/elementosempresas.js
@@ -0,0 +1,5 @@
+export const Elementosempresas = [
+    {empresaicon:"fa-solid fa-bars", tipoTarjeta:"Dashboard", numeroEmpresas:85},
+    {empresaicon:"fa-solid fa-bars", tipoTarjeta:"Dashboard", numeroEmpresas:55},
+    {empresaicon:"fa-solid fa-bars", tipoTarjeta:"Dashboard", numeroEmpresas:5},
+  ]
