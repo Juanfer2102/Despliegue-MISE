f163618ba4b364b353016f896326ce7c5376fe30
Aleej0 <alejohan1130@gmail.com>
2024-09-18 11:10:03 -0500

    ?

--- FrontEnd/src/pages/dashboard/dashboardemp.jsx
+++ FrontEnd/src/pages/dashboard/dashboardemp.jsx
@@ -1,25 +1,19 @@
 import React, { useEffect, useState } from "react";
+import { motion } from "framer-motion"; // Importa framer-motion
 import GoBack from "../../components/inputs/goback/GoBack.jsx";
 import LayoutDashboard from "../../layouts/LayoutDashboard.jsx";
-import Grafica from "../../components/inputs/barcharts/graficabarras.jsx";
-import { ProgressCircleHero } from "../../components/inputs/porcentaje/porcentaje.jsx";
-import TablasEmpestado from "../../components/tablas/tablasEmpestado.jsx";
-import TablasEmpresaporc from "../../components/tablas/tablasEmpresaporc.jsx";
-import TarjetasModulos from "../../components/tarjetasdashboard/tarjetasModulos.jsx";
 import TarjetasTema from "../../components/tarjetasdashboard/tarjetasTema.jsx";
 import TablaPreguntas from "../../components/tablas/tablaempregunta.jsx";
 import ValidacionDeSueños from "../../components/tablas/validacionSuenos.jsx";
 import { useParams } from 'react-router-dom';
 
 const DashboardEmp = () => {
-
   const { nit } = useParams();
   const [empresa, setEmpresa] = useState({});
   const [calificacionesBajas, setCalificacionesBajas] = useState([]);
   const [diagnosticos, setDiagnosticos] = useState([]);
   const [calificaciones, setCalificaciones] = useState([]);
   const [postulante, setPostulante] = useState([]);
-
 
   useEffect(() => {
     // Obtener información de la empresa
@@ -57,35 +51,13 @@ const DashboardEmp = () => {
       });
   }, [nit]);
 
-  useEffect (() => {
+  useEffect(() => {
     fetch(`http://localhost:8000/api/v2/postulante/num/${nit}/`)
-    .then(response => response.json())
-    .then(data =>{
-      setPostulante(data);
-    })
+      .then(response => response.json())
+      .then(data => {
+        setPostulante(data);
+      });
   }, [nit]);
-
-
-  // Estilos en JSX
-  const styles = {
-    customScrollbar: {
-      scrollbarWidth: '13px',
-      scrollbarColor: '#888 #262b32',
-    },
-    customScrollbarTrack: {
-      background: '#262b32',
-      borderRadius: '12px',
-    },
-    customScrollbarThumb: {
-      background: '#888',
-      borderRadius: '10px',
-    },
-    customScrollbarThumbHover: {
-      background: '#555',
-    }
-  };
-
-  console.log(calificaciones)
 
   return (
     <LayoutDashboard title="Dashboard">
@@ -95,18 +67,35 @@ const DashboardEmp = () => {
           <div className="bg-greyBg flex flex-col py-2 xl:gap-5 lg:gap-5 gap-4 w-full xl:h-full px-4 lg:px-12 xl:px-12 pt-4 xl:pt-6">
             <GoBack text={`Empresas Registradas / ${empresa.nombre_empresa}`} />
 
-            <div className="flex flex-col xl:flex-row lg:flex-row xl:gap-[5rem] lg:gap-[3rem] gap-10">
+            <motion.div
+              className="flex flex-col xl:flex-row lg:flex-row xl:gap-[5rem] lg:gap-[3rem] gap-10"
+              initial={{ opacity: 0, y: 20 }}
+              animate={{ opacity: 1, y: 0 }}
+              transition={{ duration: 0.5 }}
+            >
               <div className="flex flex-col lg:h-[28rem] lg:flex-row gap-5 w-full xl:w-auto lg:w-auto">
                 <TarjetasTema />
               </div>
-            </div>
-            <div className="flex flex-col xl:flex-row lg:flex-row gap-8 max-md:pb-2 xl:pb-2 lg:pb-2 xl:justify-between h-full">
+            </motion.div>
+
+            <motion.div
+              className="flex flex-col xl:flex-row lg:flex-row gap-8 max-md:pb-2 xl:pb-2 lg:pb-2 xl:justify-between h-full"
+              initial={{ opacity: 0, x: -20 }}
+              animate={{ opacity: 1, x: 0 }}
+              transition={{ duration: 0.5 }}
+            >
               <TablaPreguntas calificaciones={calificaciones} />
               <ValidacionDeSueños diagnosticos={diagnosticos} />
-            </div>
+            </motion.div>
+
             <div className="flex xl:flex-row lg:flex-row flex-col xl:gap-[5rem] lg:gap-[5rem]">
-              <div className="w-full">
-                <h1 className="text-lg sm:text-xl font-bold mb-1 sm:mb-2 text-white p-3">Informacion Postulante</h1>
+              <motion.div
+                className="w-full"
+                initial={{ opacity: 0, scale: 0.95 }}
+                animate={{ opacity: 1, scale: 1 }}
+                transition={{ duration: 0.5 }}
+              >
+                <h1 className="text-lg sm:text-xl font-bold mb-1 sm:mb-2 text-white p-3">Información Postulante</h1>
                 <div className="bg-greyBlack p-3 sm:p-5 rounded-xl mb-4">
                   <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-2 gap-4">
                     <div className="space-y-4">
@@ -141,10 +130,15 @@ const DashboardEmp = () => {
                     </div>
                   </div>
                 </div>
-              </div>
+              </motion.div>
 
-              <div className="w-full">
-                <h1 className="text-lg sm:text-xl font-bold mb-1 sm:mb-2 text-white p-3">Informacion Empresa</h1>
+              <motion.div
+                className="w-full"
+                initial={{ opacity: 0, scale: 0.95 }}
+                animate={{ opacity: 1, scale: 1 }}
+                transition={{ duration: 0.5 }}
+              >
+                <h1 className="text-lg sm:text-xl font-bold mb-1 sm:mb-2 text-white p-3">Información Empresa</h1>
                 <div className="bg-greyBlack p-3 sm:p-5 rounded-xl mb-4">
                   <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-2 gap-4">
                     <div className="space-y-4">
@@ -157,13 +151,13 @@ const DashboardEmp = () => {
                         <p className="text-principalGreen font-semibold">{empresa.fecha_creacion}</p>
                       </div>
                       <div>
-                        <h2 className="text-lg sm:text-xl font-bold mb-1 sm:mb-2 text-white">Costos el Ultimo Año</h2>
+                        <h2 className="text-lg sm:text-xl font-bold mb-1 sm:mb-2 text-white">Costos el Último Año</h2>
                         <p className="text-principalGreen font-semibold">{empresa.costos_ult_ano}</p>
                       </div>
                     </div>
                     <div className="space-y-4">
                       <div>
-                        <h2 className="text-lg sm:text-xl font-bold mb-1 sm:mb-2 text-white">Razon Social</h2>
+                        <h2 className="text-lg sm:text-xl font-bold mb-1 sm:mb-2 text-white">Razón Social</h2>
                         <p className="text-principalGreen font-semibold">{empresa.razon_social}</p>
                       </div>
                       <div>
@@ -171,25 +165,27 @@ const DashboardEmp = () => {
                         <p className="text-principalGreen font-semibold">{empresa.celular}</p>
                       </div>
                       <div>
-                        <h2 className="text-lg sm:text-xl font-bold mb-1 sm:mb-2 text-white">Ventas el Ultimo Año</h2>
+                        <h2 className="text-lg sm:text-xl font-bold mb-1 sm:mb-2 text-white">Ventas el Último Año</h2>
                         <p className="text-principalGreen font-semibold">{empresa.ventas_ult_ano}</p>
                       </div>
                     </div>
                     <div className="space-y-4">
                       <div>
-                        <h2 className="text-lg sm:text-xl font-bold mb-1 sm:mb-2 text-white">NIT</h2>
-                        <p className="text-principalGreen font-semibold">{empresa.nit}</p>
+                        <h2 className="text-lg sm:text-xl font-bold mb-1 sm:mb-2 text-white">Dirección</h2>
+                        <p className="text-principalGreen font-semibold">{empresa.direccion}</p>
                       </div>
-                    </div>
-                    <div className="space-y-4">
                       <div>
-                        <h2 className="text-lg sm:text-xl font-bold mb-1 sm:mb-2 text-white">Empleados Permanentes</h2>
+                        <h2 className="text-lg sm:text-xl font-bold mb-1 sm:mb-2 text-white">Teléfono</h2>
+                        <p className="text-principalGreen font-semibold">{empresa.celular }</p>
+                      </div>
+                      <div>
+                        <h2 className="text-lg sm:text-xl font-bold mb-1 sm:mb-2 text-white">Número de Empleados</h2>
                         <p className="text-principalGreen font-semibold">{empresa.empleados_perm}</p>
                       </div>
                     </div>
                   </div>
                 </div>
-              </div>
+              </motion.div>
             </div>
           </div>
         </div>
