901c3415215e770c5d7ffa431b49c274dc46d255
Aleej0 <alejohan1130@gmail.com>
2024-09-25 21:08:36 -0500

    push remake design

--- FrontEnd/src/components/tarjetasdashboard/tarjetasTemaFinal.jsx
+++ FrontEnd/src/components/tarjetasdashboard/tarjetasTemaFinal.jsx
@@ -80,14 +80,13 @@ const TarjetasTemaFinal = ({ nit }) => {
     };
 
     return (
-        <div className="bg-greyBlack rounded-xl p-4 text-white w-full">
+        <>
+            <div className="bg-greyBlack rounded-xl p-4 text-white w-full">
 
-            <div className="flex flex-row justify-around py-2">
-                <h2 className="text-2xl font-bold mb-4 text-center">Temas evaluados</h2>
-                <h2 className="text-2xl font-bold mb-4 text-center">Sueños evaluados</h2>
-            </div>
-            <div className="grid grid-cols-2 h-[40rem]">
-                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-6 p-2 max-h-[35rem] overflow-y-auto" style={styles.customScrollbar}>
+                <div className="flex flex-row justify-around py-2">
+                    <h2 className="text-2xl font-bold mb-4 text-center">Temas evaluados</h2>
+                </div>
+                <div className="grid grid-cols-1 sm:grid-cols-2 gap-6 p-2 max-h-[35rem] overflow-y-auto" style={styles.customScrollbar}>
                     {temas.map((tema) => (
                         <div
                             key={tema.id_tema}
@@ -102,76 +101,83 @@ const TarjetasTemaFinal = ({ nit }) => {
                             <div className="w-full h-full" style={{ aspectRatio: '1 / 1' }}></div>
                         </div>
                     ))}
-                </div>
-                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-6 p-2 max-h-[35rem] overflow-y-auto" style={styles.customScrollbar}>
-                    {temas.map((tema) => (
-                        <div
-                            key={tema.id_tema}
-                            className="relative flex items-center justify-center bg-darkGrey rounded-lg border border-principalGreen shadow-lg hover:shadow-xl hover:bg-principalGreen transition-all duration-300 cursor-pointer"
-                            onClick={() => openModal(tema)}
-                        >
-                            <div className="absolute inset-0 p-4 flex items-center justify-center flex-col">
-                                <p className="text-lg font-semibold text-center">{tema.objetivo}</p>
-                                <p className="font-bold text-principalGreen">{tema.fecha_inicio} - {tema.fecha_fin}</p>
-                                <p className={`text-${tema.estado === 0 ? 'amarillo' : tema.estado === 1 ? 'principalGreen' : 'red'}`}>{tema.criterio}</p>
-                            </div>
-                            <div className="w-full h-full" style={{ aspectRatio: '1 / 1' }}></div>
-                        </div>
-                    ))}
-                </div>
-            </div>
 
-            {isModalOpen && selectedTema && (
-                <div className="fixed inset-0 flex items-center justify-center z-50 bg-black bg-opacity-50">
-                    <div className="bg-greyBlack text-white p-6 rounded-lg shadow-lg w-full h-[35rem] max-w-2xl overflow-auto">
-                        <h3 className="text-2xl font-bold mb-4">Detalles del Tema</h3>
-                        <div className="grid grid-cols-1 md:grid-cols-2 gap-4 max-h-[25rem] overflow-y-auto" style={styles.customScrollbar}>
-                            {/* Información del tema */}
-                            <div>
-                                <p className="text-lg font-semibold mb-2"><strong>Módulo:</strong></p>
-                                <p className="mb-4">{selectedTema.id_modulo}</p>
+                </div>
+
+                {isModalOpen && selectedTema && (
+                    <div className="fixed inset-0 flex items-center justify-center z-50 bg-black bg-opacity-50">
+                        <div className="bg-greyBlack text-white p-6 rounded-lg shadow-lg w-full h-[35rem] max-w-2xl overflow-auto">
+                            <h3 className="text-2xl font-bold mb-4">Detalles del Tema</h3>
+                            <div className="grid grid-cols-1 md:grid-cols-2 gap-4 max-h-[25rem] overflow-y-auto" style={styles.customScrollbar}>
+                                {/* Información del tema */}
+                                <div>
+                                    <p className="text-lg font-semibold mb-2"><strong>Módulo:</strong></p>
+                                    <p className="mb-4">{selectedTema.id_modulo}</p>
+                                </div>
+                                <div>
+                                    <p className="text-lg font-semibold mb-2"><strong>Sesión:</strong></p>
+                                    <p className="mb-4">{selectedTema.num_sesion}</p>
+                                </div>
+                                <div>
+                                    <p className="text-lg font-semibold mb-2"><strong>Objetivo:</strong></p>
+                                    <p className="mb-4">{selectedTema.objetivo}</p>
+                                </div>
+                                <div>
+                                    <p className="text-lg font-semibold mb-2"><strong>Alcance:</strong></p>
+                                    <p className="mb-4">{selectedTema.alcance}</p>
+                                </div>
+                                <div>
+                                    <p className="text-lg font-semibold mb-2"><strong>Contenido:</strong></p>
+                                    <p className="mb-4">{selectedTema.contenido}</p>
+                                </div>
+                                <div>
+                                    <p className="text-lg font-semibold mb-2"><strong>Conferencista:</strong></p>
+                                    <p className="mb-4">{selectedTema.conferencista}</p>
+                                </div>
+                                <div>
+                                    <p className="text-lg font-semibold mb-2"><strong>Fecha de inicio:</strong></p>
+                                    <p className="mb-4">{selectedTema.fecha_inicio}</p>
+                                </div>
+                                <div>
+                                    <p className="text-lg font-semibold mb-2"><strong>Fecha de Fin</strong></p>
+                                    <p className="mb-4">{selectedTema.fecha_fin}</p>
+                                </div>
+                                <div>
+                                    <p className="text-lg font-semibold mb-2"><strong>Ubicación:</strong></p>
+                                    <p className="mb-4">{selectedTema.ubicacion}</p>
+                                </div>
                             </div>
-                            <div>
-                                <p className="text-lg font-semibold mb-2"><strong>Sesión:</strong></p>
-                                <p className="mb-4">{selectedTema.num_sesion}</p>
-                            </div>
-                            <div>
-                                <p className="text-lg font-semibold mb-2"><strong>Objetivo:</strong></p>
-                                <p className="mb-4">{selectedTema.objetivo}</p>
-                            </div>
-                            <div>
-                                <p className="text-lg font-semibold mb-2"><strong>Alcance:</strong></p>
-                                <p className="mb-4">{selectedTema.alcance}</p>
-                            </div>
-                            <div>
-                                <p className="text-lg font-semibold mb-2"><strong>Contenido:</strong></p>
-                                <p className="mb-4">{selectedTema.contenido}</p>
-                            </div>
-                            <div>
-                                <p className="text-lg font-semibold mb-2"><strong>Conferencista:</strong></p>
-                                <p className="mb-4">{selectedTema.conferencista}</p>
-                            </div>
-                            <div>
-                                <p className="text-lg font-semibold mb-2"><strong>Fecha de inicio:</strong></p>
-                                <p className="mb-4">{selectedTema.fecha_inicio}</p>
-                            </div>
-                            <div>
-                                <p className="text-lg font-semibold mb-2"><strong>Fecha de Fin</strong></p>
-                                <p className="mb-4">{selectedTema.fecha_fin}</p>
-                            </div>
-                            <div>
-                                <p className="text-lg font-semibold mb-2"><strong>Ubicación:</strong></p>
-                                <p className="mb-4">{selectedTema.ubicacion}</p>
-                            </div>
+                            <button className="mt-6 px-4 py-2 bg-red text-white rounded-lg hover:bg-white hover:text-red transition-colors duration-300" onClick={closeModal}>
+                                Cerrar
+                            </button>
                         </div>
-                        <button className="mt-6 px-4 py-2 bg-red text-white rounded-lg hover:bg-white hover:text-red transition-colors duration-300" onClick={closeModal}>
-                            Cerrar
-                        </button>
                     </div>
-                </div>
-            )}
+                )}
 
-        </div>
+            </div>
+            <div className="bg-greyBlack rounded-xl p-4 text-white w-full">
+                <div className="flex flex-row justify-around py-2">
+                    <h2 className="text-2xl font-bold mb-4 text-center">Sueños evaluados</h2>
+                </div>
+                <div className="grid grid-cols-2 gap-6 p-2 max-h-[35rem] overflow-y-auto" style={styles.customScrollbar}>
+                    {temas.map((tema) => (
+                        <div
+                            key={tema.id_tema}
+                            className="relative flex items-center justify-center bg-darkGrey rounded-lg border border-principalGreen shadow-lg hover:shadow-xl hover:bg-principalGreen transition-all duration-300 cursor-pointer"
+                            onClick={() => openModal(tema)}
+                        >
+                            <div className="absolute inset-0 p-4 flex items-center justify-center flex-col">
+                                <p className="text-lg font-semibold text-center">{tema.objetivo}</p>
+                                <p className="font-bold text-principalGreen">{tema.fecha_inicio} - {tema.fecha_fin}</p>
+                                <p className={`text-${tema.estado === 0 ? 'amarillo' : tema.estado === 1 ? 'principalGreen' : 'red'}`}>{tema.criterio}</p>
+                            </div>
+                            <div className="w-full h-full" style={{ aspectRatio: '1 / 1' }}></div>
+                        </div>
+                    ))}
+                </div>
+            </div>
+
+        </>
     );
 };
 
