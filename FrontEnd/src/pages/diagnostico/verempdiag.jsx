bf38a6228a866152154b9df2b38aeb6a06d0278e
Juanfer2102 <160734739+Juanfer2102@users.noreply.github.com>
2024-10-02 10:57:49 -0500

    www

--- FrontEnd/src/pages/diagnostico/verempdiag.jsx
+++ FrontEnd/src/pages/diagnostico/verempdiag.jsx
@@ -34,7 +34,7 @@
 import React from 'react';
 import LayoutDashboard from '../../layouts/LayoutDashboard';
 import TablaDiagnostico from '../../components/tablas/tablaDiagnostico';
-import GoBack from '../../components/inputs/goback/GoBack';
+import BackButton from '../../components/inputs/goback/BackButton';
 
 const VerEmpDiag = () => {
   return (
@@ -45,7 +45,7 @@ const VerEmpDiag = () => {
           <div className="bg-greyBg flex flex-col py-2 xl:gap-5 lg:gap-5 w-full xl:h-full px-4 lg:px-4 xl:px-4 pt-4 xl:pt-2 lg:pt-2">
             <div className="flex flex-col gap-6 p-4 sm:p-6 md:p-8 rounded-md">
               <div className="flex flex-col gap-2 h-full rounded-xl">
-                <GoBack text={"Diagnóstico de Empresas"} />
+                <BackButton text={"Diagnóstico de Empresas"} />
                 <div className="h-full overflow-x-auto rounded-xl">
                   <TablaDiagnostico />
                 </div>
