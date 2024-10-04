b920f6aebf7c455e38c2ef5add33fa916d4ef3bb
Juanfer2102 <160734739+Juanfer2102@users.noreply.github.com>
2024-10-04 11:19:53 -0500

    q

--- Back_End/users/serializer.py
+++ Back_End/users/serializer.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from django.contrib.auth.tokens import default_token_generator
+from users.tokens import default_token_generator
 from django.utils.http import urlsafe_base64_decode
 from django.shortcuts import get_object_or_404
 from .models import Temas, TemasPreguntas, SuenosConcretados, Autoevaluacion, DiagnosticoEmpresarialSuenos, Calificaciones, Diagnostico1, Modulo1, Respuesta1, CalificacionModulo, ModuloAutoevaluacion, Empresas, Modulos, Postulante, Preguntas, Programas, Registros, Rol, Suenos, Talleres, Usuario
